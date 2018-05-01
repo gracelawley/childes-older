026794c6da6cbc66a6a4c3bcbcfb5e58b89a7b75
Grace Lawley <lawley@ohsu.edu>
2018-05-01 13:06:50 -0700

    Add transcripts and utterances for MacWhinney, Suppes, Sachs, and Weist

---
+++ R/macwhinney/01_macwhinney_get-transcripts.R
@@ -0,0 +1,30 @@
+library(here)
+library(dplyr)
+library(childesr)
+
+
+## Output path -----------------------------------------------------
+out_path = here("data/raw/macwhinney/macwhinney_transcripts.csv")
+## -----------------------------------------------------------------
+
+# Get transcript names
+transcripts <- get_transcripts(collection = 'Eng-NA',
+                               corpus = 'MacWhinney')
+
+# The database version used
+version <- get_database_version()
+
+# Adding database version info to dataframe
+transcripts <- transcripts %>% 
+  mutate(db_version = version)
+
+# Selecting only needed columns=
+transcripts <- transcripts %>% 
+  select(db_version, collection = collection_name,
+         corpus = corpus_name, target_child = target_child_name,
+         target_child_id, sex = target_child_sex, 
+         age = target_child_age, date, 
+         file = filename, transcript_id)
+
+# Write output file
+write.csv(transcripts, out_path, row.names = FALSE)
